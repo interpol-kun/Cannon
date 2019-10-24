9b83f2310de2b1f8b98bc59e871f8f874fe21de4
Alex <alex456213798@yandex.ru>
2019-10-24 19:15:50 +0300

    menu

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public void PausePressed()
+    {
+        Time.timeScale = 0f;
+    }
+    public void ContinuePressed()
+    {
+        Time.timeScale = 1f;
+    }
+    public void MenuPressed()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("Menu");
+    }
+    public void ExitPressed()
+    {
+        Application.Quit();
+        Debug.Log("EXIT");
+    }
+
+}
